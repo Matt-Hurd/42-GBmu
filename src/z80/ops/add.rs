64d734aefcb1f9b6319dc3aba4a81e70b07bd09c
Christopher Serr <christopher.serr@gmail.com>
2017-01-06 13:10:25 +0100

    Small Code Cleanup

--- src/z80/ops/add.rs
+++ src/z80/ops/add.rs
@@ -1,15 +1,14 @@
 use z80::Z80;
 
-//Need to check for overflow, not sure if there's a better way to check
 pub fn add_r_e(z80: &mut Z80) {
-    let temp: u16 = z80.r.a as u16 + z80.r.e as u16;
+    let (sum, overflow) = z80.r.a.overflowing_add(z80.r.e);
     z80.r.f = 0;
-    if (temp & 255 as u16) != 0 {
+    if sum != 0 {
         z80.r.f |= 0x80;
     }
-    if temp > 255 {
+    if overflow {
         z80.r.f |= 0x10;
     }
-    z80.r.a = temp as u8 & 255;
+    z80.r.a = sum;
     z80.set_register_clock(1);
 }
